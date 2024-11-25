31de6500a3b9177f9734bfaca87fe0e3394b16ae
Gilvan Gabriel <93411051+GilvanGabrielCA@users.noreply.github.com>
2024-11-25 11:46:01 -0400

    first commit

---
+++ bin/swiftcar.dart
@@ -0,0 +1,5 @@
+import 'package:swiftcar/swiftcar.dart' as swiftcar;
+
+void main(List<String> arguments) {
+  print('Hello world: ${swiftcar.calculate()}!');
+}
---
+++ test/swiftcar_test.dart
@@ -0,0 +1,8 @@
+import 'package:swiftcar/swiftcar.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
